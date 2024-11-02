573e55986f0da01ce24d2b0990748d0efb8e87e1
ha-ccoon <codeminhasohn@gmail.com>
2024-11-02 11:33:20 +0900

    chore: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  await app.listen(8000);
+
+  console.log('Server listen to 8000');
 }
 bootstrap();
