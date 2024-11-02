6ba63cf6e34e9a1cf895ef8158aab42c4d70cda6
ha-ccoon <codeminhasohn@gmail.com>
2024-11-02 11:29:41 +0900

    chore: add chat entity

--- src/chat/entities/chat.entity.ts
+++ src/chat/entities/chat.entity.ts
@@ -1,1 +1,31 @@
-export class Chat {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { HydratedDocument } from 'mongoose';
+
+export type ChatDocument = HydratedDocument<Chat>;
+
+@Schema()
+export class Chat {
+  @Prop()
+  senderId: string;
+
+  @Prop()
+  receiverId: string;
+
+  @Prop()
+  messageId: string;
+
+  @Prop()
+  message: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  deletedAt: Date;
+}
+
+export const ChatSchema = SchemaFactory.createForClass(Chat);
