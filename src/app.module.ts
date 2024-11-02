797491c3c4d69b30be75ca955579a0ebe0ca3b5b
ha-ccoon <codeminhasohn@gmail.com>
2024-11-02 11:33:08 +0900

    chore: implement mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://kunatalk:kunatalk@mongo:27017'),
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Model } from 'mongoose';
 
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
+import { Chat } from './entities/chat.entity';
 
 @Injectable()
 export class ChatService {
+  constructor(@InjectModel(Chat.name) private chatModel: Model<Chat>) {}
+
   create(createChatDto: CreateChatDto) {
     return 'This action adds a new chat';
   }
