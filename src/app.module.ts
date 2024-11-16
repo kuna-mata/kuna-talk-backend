f4528e1ce4e33151696c81cd4894db091318ae26
ha-ccoon <codeminhasohn@gmail.com>
2024-11-16 23:39:57 +0900

    chore: implement ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import envConfig from './config/env.config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://kunatalk:kunatalk@mongo:27017'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      load: [envConfig],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+      }),
+      inject: [ConfigService],
+    }),
     ChatModule,
   ],
   controllers: [AppController],
