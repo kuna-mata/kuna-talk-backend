3eba291423f1188bd804e7e09214d332c386f28b
ha-ccoon <codeminhasohn@gmail.com>
2024-11-17 13:32:18 +0900

    chore: configure mobile connection

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,4 +2,12 @@ export default () => ({
   database: {
     uri: process.env.DATABASE_URL,
   },
+  mobile: {
+    android: {
+      uri: process.env.ANDROID_URL,
+    },
+    device: {
+      uri: process.env.DEVICE_URL,
+    },
+  },
 });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const configService = app.get(ConfigService);
+
+  const enabledCorsUrls = [
+    configService.get<string>('mobile.android.uri'),
+    configService.get<string>('mobile.device.uri'),
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: enabledCorsUrls,
     credentials: true,
   });
 
